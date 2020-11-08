fb60bb3f234fa04d2debb09ff17b7f07380e822b
Sebastian Rakel <sebastian@devunit.eu>
2020-11-08 19:24:19 +0100

    Switch to github actions and fix tests

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe 'borg' do
+describe 'teamspeak' do
   let :node do
     'rspec.puppet.com'
   end
@@ -15,7 +15,7 @@ describe 'borg' do
         it { is_expected.to compile.with_all_deps }
 
         it { is_expected.to contain_file('/opt/teamspeak3') }
-        it { is_expected.to contain_file('/opt/teamspeak/downloads3') }
+        it { is_expected.to contain_file('/opt/teamspeak3/downloads') }
         it { is_expected.to contain_class('teamspeak::install') }
         it { is_expected.to contain_class('teamspeak::service') }
         it { is_expected.to contain_user('teamspeak') }
